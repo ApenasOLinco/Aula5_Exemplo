4d0975e3410ad3da23c1e527376ca762909249ed
ApenasOLinco <lincoln.matos1003@gmail.com>
2024-09-18 10:48:35 -0300

    Script adicionado

---
+++ js/codigos.js
@@ -0,0 +1,10 @@
+console.log("O arquivo de script foi lido!");
+// alert("oi");
+
+var num1 = 10;
+var num2 = 5;
+var resultado = num1 + num2;
+console.log(`A soma de ${num1} e ${num2} é ${resultado}`);
+
+let x = parseInt(prompt("Digite um número inteiro: "));
+console.log(`${x} ${typeof x}`);
